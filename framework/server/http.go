ed02af3502af8c5204322be21141a4cd31e9434c
tsfans <hu382018@outlook.com>
2025-08-26 11:08:32 +0800

    feat: add http timeout.

--- framework/server/http.go
+++ framework/server/http.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tsfans/go/framework/logger"
 )
@@ -11,16 +14,28 @@ var (
 )
 
 type HttpServer struct {
-	*gin.Engine
-	listenAddr string
+	handler *gin.Engine
+	srv     *http.Server
 }
 
 func InitHttpServer() *HttpServer {
-	httpServer = &HttpServer{Engine: gin.Default(), listenAddr: ":8080"}
+	httpServer = &HttpServer{handler: gin.Default()}
+	ip := "0.0.0.0"
+	port := "8080"
+	address := fmt.Sprintf("%s:%s", ip, port)
+	httpServer.srv = &http.Server{
+		Addr:           address,
+		Handler:        httpServer.handler,
+		ReadTimeout:    5 * 60 * 1000,
+		WriteTimeout:   5 * 60 * 1000,
+		MaxHeaderBytes: 1 << 20,
+	}
 	return httpServer
 }
 
 func (hs HttpServer) Run() {
-	log.Infof("http server is running at %s", hs.listenAddr)
-	hs.Engine.Run(hs.listenAddr)
+	log.Infof("http server is running at %s", hs.srv.Addr)
+	if err := hs.srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("http server start error: %s", err)
+	}
 }
